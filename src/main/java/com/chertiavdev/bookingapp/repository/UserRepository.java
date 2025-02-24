6cabde92f42329234d8ba53b862290586c1882e1
Andrey Chertykovtsev <chertiav@gmail.com>
2025-02-24 15:15:56 +0100

    Add JWT-based authentication and security configuration - implemented JwtAuthenticationFilter; - implemented SecurityConfig; - implemented CustomUserDetailsService.

---
+++ src/main/java/com/chertiavdev/bookingapp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.chertiavdev.bookingapp.repository;
+
+import com.chertiavdev.bookingapp.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = "roles")
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/chertiavdev/bookingapp/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.chertiavdev.bookingapp.security;
+
+import com.chertiavdev.bookingapp.exception.EntityNotFoundException;
+import com.chertiavdev.bookingapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Can't find user by email: " + email));
+    }
+}
