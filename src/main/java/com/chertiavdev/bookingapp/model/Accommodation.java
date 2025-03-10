6293bb6f5ba1522b7987e9e1fb80edeae959b23f
Andrey Chertykovtsev <chertiav@gmail.com>
2025-03-10 13:06:54 +0100

    Add accommodations and amenities association: - introduced `accommodations` and `accommodation_amenities` tables to the database with appropriate constraints and relationships; - implemented the `Accommodation` entity in the Java model, including an enum for types and associations with addresses and amenities; - updated changelogs to include new table creation and default value for `is_deleted` in `addresses`.

---
+++ src/main/java/com/chertiavdev/bookingapp/model/Accommodation.java
@@ -0,0 +1,76 @@
+package com.chertiavdev.bookingapp.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE accommodations SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "accommodations")
+public class Accommodation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address location;
+
+    @Column(nullable = false)
+    private String size;
+
+    @EqualsAndHashCode.Exclude
+    @ManyToMany
+    @ToString.Exclude
+    @JoinTable(
+            name = "accommodation_amenities",
+            joinColumns = @JoinColumn(name = "accommodation_id"),
+            inverseJoinColumns = @JoinColumn(name = "amenity_id")
+    )
+    private Set<Amenity> amenities = new HashSet<>();
+
+    @Column(nullable = false)
+    private BigDecimal dailyRate;
+
+    @Column(nullable = false)
+    private Integer availability;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    public enum Type {
+        HOUSE,
+        APARTMENT,
+        CONDO,
+        VACATION_HOME
+    }
+}
