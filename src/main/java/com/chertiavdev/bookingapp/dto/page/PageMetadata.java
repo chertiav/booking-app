6b5075fd08d5eca1064fe76f56acb0f66fbaa9a0
Andrey Chertykovtsev <chertiav@gmail.com>
2025-03-13 12:45:02 +0100

    Add pagination DTOs: PageMetadata and PageResponse - introduced `PageMetadata` and `PageResponse` DTOs to encapsulate paginated data and metadata.

---
+++ src/main/java/com/chertiavdev/bookingapp/dto/page/PageMetadata.java
@@ -0,0 +1,19 @@
+package com.chertiavdev.bookingapp.dto.page;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Schema(description = "Metadata about the current page and total data count")
+public class PageMetadata {
+    @Schema(description = "Current page number")
+    private int currentPage;
+    @Schema(description = "Number of items per page")
+    private int pageSize;
+    @Schema(description = "Total number of pages")
+    private int totalPageCount;
+    @Schema(description = "Total number of elements")
+    private long totalElementCount;
+}
---
+++ src/main/java/com/chertiavdev/bookingapp/dto/page/PageResponse.java
@@ -0,0 +1,30 @@
+package com.chertiavdev.bookingapp.dto.page;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+@Data
+@AllArgsConstructor
+@Schema(description = "Paginated response containing content and metadata")
+public class PageResponse<T> {
+    @Schema(description = "List of items on the current page")
+    private List<T> content;
+    @Schema(description = "Metadata about pagination")
+    private PageMetadata metadata;
+
+    public static <T> PageResponse<T> of(Page<T> page) {
+        return new PageResponse<>(page.getContent(), getMetaData(page));
+    }
+
+    private static <T> PageMetadata getMetaData(Page<T> page) {
+        return new PageMetadata(
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalPages(),
+                page.getTotalElements()
+        );
+    }
+}
