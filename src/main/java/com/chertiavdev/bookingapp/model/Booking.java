91371bed49c57704631f7620fe10ca01d8458e35
Andrey Chertykovtsev <chertiav@gmail.com>
2025-03-21 13:08:51 +0100

    Add bookings table and Booking entity

---
+++ src/main/java/com/chertiavdev/bookingapp/model/Booking.java
@@ -0,0 +1,65 @@
+package com.chertiavdev.bookingapp.model;
+
+import com.chertiavdev.bookingapp.validation.json.TypeDeserializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Setter
+@ToString
+@SQLDelete(sql = "UPDATE bookings SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+@EqualsAndHashCode
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate checkIn;
+
+    @Column(nullable = false)
+    private LocalDate checkOut;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accommodation_id", nullable = false)
+    private Accommodation accommodation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+
+    @JsonDeserialize(using = TypeDeserializer.class)
+    public enum Status {
+        PENDING,
+        CONFIRMED,
+        CANCELLED,
+        EXPIRED
+    }
+}
